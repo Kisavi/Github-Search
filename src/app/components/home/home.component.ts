d140347ead25ec2d04562d1d0b6eb84a437c4484
Kisavi <deniskisavi@gmail.com>
2022-04-14 20:40:30 +0300

    call the service by importing it in our home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileRequestService } from 'src/app/services/profile-request/profile-request.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private profileService:ProfileRequestService) { }
 
   ngOnInit(): void {
   }
