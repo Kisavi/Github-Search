d88a77ca6aba2f23c7641f811431ea0dd3f44c6a
Kisavi <deniskisavi@gmail.com>
2022-04-15 00:24:55 +0300

    call our service component in our repository component by importing it at the top and injectimg it as a parameter in the constructor

--- src/app/components/repositories/repositories.component.ts
+++ src/app/components/repositories/repositories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RepositoryRequestService } from 'src/app/services/repository-request/repository-request.service';
 
 @Component({
   selector: 'app-repositories',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoriesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private repositoryService:RepositoryRequestService) { }
 
   ngOnInit(): void {
   }
