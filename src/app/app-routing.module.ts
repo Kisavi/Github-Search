51aa2bdaeafc3f47d21b298afa0a686658d9eedb
Kisavi <deniskisavi@gmail.com>
2022-04-14 13:42:41 +0300

    import our http clients in our app.module to make it available anywhere in our application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RepositoriesComponent } from './components/repositories/repositories.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'home', component: HomeComponent},
-  { path: 'repositories', component: RepositoriesComponent},
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'repositories', component: RepositoriesComponent },
+  { path: '**', component: ErrorPageComponent },
 ];
 
 @NgModule({
