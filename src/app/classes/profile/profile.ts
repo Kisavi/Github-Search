c8120271b397902de5519fd8b176996c099cdade
Kisavi <deniskisavi@gmail.com>
2022-04-14 14:11:43 +0300

    create constructors to define how we want our profle and repository instances created

--- src/app/classes/profile/profile.ts
+++ src/app/classes/profile/profile.ts
@@ -1,2 +1,9 @@
 export class Profile {
+    constructor(
+        public image: string,
+        public fullName: string,
+        public userName: string,
+        public followers: number,
+        public following: number,
+    ) { }
 }
--- src/app/classes/repository/repository.ts
+++ src/app/classes/repository/repository.ts
@@ -1,2 +1,10 @@
 export class Repository {
+    constructor(
+        public repositoryName: string,
+        public description: string,
+        public lastUpdate: Date,
+        public language: string,
+        public star: string,
+        public fork: string,
+    ){}
 }
