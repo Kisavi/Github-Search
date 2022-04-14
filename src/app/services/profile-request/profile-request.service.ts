ff2862f53e3c3a56aa23c282bf422775053cdbf6
Kisavi <deniskisavi@gmail.com>
2022-04-14 15:35:53 +0300

    create properties of both profile and repository of type Profile and Repository

--- src/app/services/profile-request/profile-request.service.ts
+++ src/app/services/profile-request/profile-request.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { profile } from '../../classes/profile';
+import { Profile } from '../../classes/profile/profile';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileRequestService {
+
+  profile!: Profile
 
   constructor() { }
 }
--- src/app/services/repository-request/repository-request.service.ts
+++ src/app/services/repository-request/repository-request.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Repository } from '../../classes/repository/repository';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepositoryRequestService {
+
+  repository!: Repository
 
   constructor() { }
 }
