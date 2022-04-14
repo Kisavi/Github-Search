db4af855ee2715f25ea0d86c83f4f20de196075b
Kisavi <deniskisavi@gmail.com>
2022-04-14 12:50:26 +0300

    configure the routes for my applicaton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'repositories', component: RepositoriesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
