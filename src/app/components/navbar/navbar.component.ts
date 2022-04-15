6ff188d07ac3413e9bcd87b00f9d6f57cbe095c3
Kisavi <deniskisavi@gmail.com>
2022-04-15 23:59:12 +0300

    St - set flex property on the repository section to properly allign the cards

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
-import { ProfileRequestService } from './../../services/profile-request/profile-request.service';
+// import { ProfileRequestService } from './../../services/profile-request/profile-request.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
@@ -9,21 +8,14 @@ import { FormBuilder } from '@angular/forms';
 })
 export class NavbarComponent implements OnInit {
 
-  searchValue(userInput:string) {
-    if(userInput) {
+  searchValue(userInput: string) {
+    if (userInput) {
       console.log(userInput)
+      // return userInput
     }
   }
 
-  constructor(
-    private _profileRequestService:ProfileRequestService
-  ) { 
-    // this.getProfileInfo()
-  }
-
-  getProfileInfo(){
-    this._profileRequestService.profileRequest()
-  }
+  // constructor(private _profileRequestService: ProfileRequestService) {}
 
   ngOnInit(): void {
   }
