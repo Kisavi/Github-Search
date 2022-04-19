2137b35718b93bc33b06a05743c82ae8e2eaf756
Kisavi <deniskisavi@gmail.com>
2022-04-19 11:59:41 +0300

    change boolean value to true in environment ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl:"https://api.github.com/users/",
   apiKey:"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"
 };
