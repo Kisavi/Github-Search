d48de6dc6e14020c135fba22e0a99122ea52141c
Kisavi <deniskisavi@gmail.com>
2022-04-19 14:36:54 +0300

    add api key and url to environment.prod.ts to make them accessible during building

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:"https://api.github.com/users/",
+  apiKey:"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"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiUrl:"https://api.github.com/users/",
   apiKey:"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"
 };
